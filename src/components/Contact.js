a6041a24033e3b2f95fef38337cae52f35393875
MohansaiRitesh <144612926+MohansaiRitesh@users.noreply.github.com>
2023-10-27 17:46:38 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: ritesh.21bce8784@vitapstudent.ac.in</p>
+      <p>Phone: +91 (9989857260)</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+// src/components/Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <div className="header-left">
+          <h1>Mohan Sai Ritesh</h1>
+          <nav>
+            <ul>
+              
+            </ul>
+          </nav>
+        </div>
+        <div className="header-right">
+          <img src="photo1.jpeg" alt="Ritesh" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
